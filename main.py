cb34ba3bf28391805bf17604430b6a1334fa7328
batenevPavel213412414325435634 <batenev.pasha@mail.ru>
2018-12-12 23:57:09 +0700

    Changes in text_preparation() and file_read()

--- main.py
+++ main.py
@@ -4,14 +4,25 @@ Batenev P.A., Grigorev A.E., Dolgih N.A.
 """
 
 
-def file_read ():
-    """ Считывает текст из файла и возвращает с типом str. """
-    # TODO
+def file_read(txt_file):
+    """ Read text from the file and return list. """
+    with open(txt_file) as f_in:
+        return f_in.read().split()
 
 
-def text_preparation():
-    """ Заменяет \n на пробелы (см. кейс 5), удаляет лишние символы и пробелы (см. задание). """
-    # TODO
+def text_preparation(text):
+    """ Removes extra characters and spaces. """
+    signs = [')', '(', '^', '%', '#', '$', ';', ':', '"', '=', '+', '-', '_', '–']
+    for i in range(len(text)):
+        if text[i][-1] in signs:
+            text[i] = text[i][:-1]
+        else:
+            text[i] = text[i]
+    for i in signs:
+        a = ' i'
+        if a in text:
+            text.replace(a, i)
+    return text
 
 
 def str_to_list():
@@ -36,9 +47,10 @@ def text_generator():
 
 def main():
     """ Main function. """
-    # TODO
+    txt_file = input()
+    text = text_preparation(file_read(txt_file))
+    print(text)
 
 
 if __name__ == '__main__':
     main()
-
