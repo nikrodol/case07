b5e933fa025fad4bd11201b46a3b4dff94f24c77
gralbert <albertgralin@gmail.com>
2018-12-12 11:45:35 +0700

    Initial commit.

---
+++ main.py
@@ -0,0 +1,44 @@
+"""Case-study #7 Generation of sentences
+Developers:
+Batenev P.A., Grigorev A.E., Dolgih N.A.
+"""
+
+
+def file_read ():
+    """ Считывает текст из файла и возвращает с типом str. """
+    # TODO
+
+
+def text_preparation():
+    """ Заменяет \n на пробелы (см. кейс 5), удаляет лишние символы и пробелы (см. задание). """
+    # TODO
+
+
+def str_to_list():
+    """ Делит текст на слова и загоняет их в список. """
+    # TODO
+
+
+def list_to_dict():
+    """ Преобразует список из str_to_list в словарь звеньев и связей. """
+    # TODO
+
+
+def start_words():
+    """ Вытаскивает из списка str_to_list стартовые слова и загоняет их в отдельный список. """
+    # TODO
+
+
+def text_generator():
+    """ Generates crazy text. """
+    # TODO
+
+
+def main():
+    """ Main function. """
+    # TODO
+
+
+if __name__ == '__main__':
+    main()
+
